245b1c61307eda77fb979af42aa6b109f49381fe
Paul McLanahan <paul@mclanahan.net>
2012-07-14 10:07:53 -0400

    Show dynamic sprint points.

--- static/js/sprint_bug_management.js
+++ static/js/sprint_bug_management.js
@@ -1,3 +1,11 @@
+function set_sprint_points(){
+    "use strict";
+    $('#current-points').html($('.sprint li')
+        .map(function(i, el){return $(el).data('points')})
+        .get().reduce(function(sum, points){
+            return sum + points;
+        }, 0));
+}
 
 $(function(){
     "use strict";
@@ -8,16 +16,19 @@ $(function(){
         placeholder: 'alert alert-info',
         forcePlaceholderSize: true
     }).disableSelection();
+    $('ul.sprint').on('sortupdate sortcreate', function(e, ui){
+        set_sprint_points();
+    });
     $('#sprint-form').on('submit', function(e){
         var bug_ids = $('.sprint li')
-            .map(function(i, el){return $(el).data('id')})
-            .toArray();
+            .map(function(i, el){return $(el).data('id')}).get();
         if (bug_ids.length) {
             $('#id_sprint_bugs').val(bug_ids.join(','));
         }
         else {
             e.preventDefault();
-            $('#form_error').removeClass('hidden');
+            $('#form_error').show();
         }
     });
+    set_sprint_points();
 });
