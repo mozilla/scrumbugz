5d1f40479b852f1157a350a6cc7a32ff11c0cf2c
Rehan Dalal <rehandalal@gmail.com>
2012-08-08 22:15:24 -0400

    Update layout of manage stories page
    Make the manage stories page match the rest of scrumbugs. Add in basic
    stats and change the way that change the mechanism for adding and
    removing bugs from a sprint.

--- static/js/sprint_bug_management.js
+++ static/js/sprint_bug_management.js
@@ -1,27 +1,46 @@
-"use strict";
-
-function set_sprint_points(){
-    var points = $('.sprint li')
-        .map(function(i, el){return $(el).data('points');}).get();
-
-    $('#current-points').html(_.reduce(points, function(sum, cpoints){
-        return sum + cpoints;
-    }));
-}
-
 $(function(){
-    $('ul.storylist').sortable({
-        connectWith: 'ul.storylist',
-        opacity: 0.7,
-        revert: 200,
-        placeholder: 'alert alert-info',
-        forcePlaceholderSize: true
-    }).disableSelection();
+    "use strict";
 
-    $('ul.sprint').on('sortupdate sortcreate', set_sprint_points);
+    function set_sprint_points(){
+        var points = $('#bugs_table tbody tr')
+              .map(function(i, el){return $(el).data('points');}).get();
+
+        if (points.length === 0) {
+            points = [0];
+        }
+
+        $('#current-points').html(_.reduce(points, function(sum, cpoints){
+            return sum + cpoints;
+        }));
+    }
+
+    function refresh_actions(){
+        $('.act-toggle-sprint').unbind('click');
+
+        $('#bugs_table .act-toggle-sprint').each(function(){
+            $(this).attr('title', 'Remove from sprint.');
+            $(this).click(function(){
+                append_bug(this, '#backlog_table');
+            });
+        });
+
+        $('#backlog_table .act-toggle-sprint').each(function(){
+            $(this).attr('title', 'Add to sprint.');
+            $(this).click(function(){
+                append_bug(this, '#bugs_table');
+            });
+        });
+    }
+
+    function append_bug(button, table) {
+        $(table).find('tbody').append($(button).parents('tr').first());
+        set_sprint_points();
+        refresh_actions();
+    }
 
     $('#sprint-form').on('submit', function(e){
-        var bug_ids = $('ul.sprint').sortable('toArray');
+        var bug_ids = $('#bugs_table tbody tr')
+              .map(function(i, el){return $(el).data('bugid');}).get();
 
         if (bug_ids.length) {
             $('#id_sprint_bugs').val(bug_ids.join(','));
@@ -33,4 +52,5 @@ $(function(){
     });
 
     set_sprint_points();
+    refresh_actions();
 });
