51b110ced132da109ac7deb8134b340d32922039
Paul McLanahan <paul@mclanahan.net>
2012-08-14 13:43:46 -0700

    Add Team to admin.

--- scrum/admin.py
+++ scrum/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from scrum.models import Project, Sprint
+from scrum.models import Project, Sprint, Team
 
 
 admin.site.register(Project)
 admin.site.register(Sprint)
+admin.site.register(Team)
