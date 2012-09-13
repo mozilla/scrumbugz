79216e6e7f0648bcb6b38f76e34ba9c9f21bc940
Paul McLanahan <paul@mclanahan.net>
2012-09-13 11:49:12 -0400

    Fix crash with no bugmail settings

--- settings/heroku.py
+++ settings/heroku.py
@@ -44,10 +44,6 @@ if SENTRY_DSN:
 
 INSTALLED_APPS += tuple(MORE_APPS)
 
-BUGMAIL_HOST = os.environ['BUGMAIL_HOST']
-BUGMAIL_USER = os.environ['BUGMAIL_USER']
-BUGMAIL_PASS = os.environ['BUGMAIL_PASS']
-
 if os.environ.get('MEMCACHIER_SERVERS'):
     os.environ['MEMCACHE_SERVERS'] = os.environ.get('MEMCACHIER_SERVERS')
     os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME')
