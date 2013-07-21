d81cfdb0cba278fff12fe74eb04d8ea7c3030ab6
Paul McLanahan <pmac@mozilla.com>
2013-07-20 23:11:13 -0400

    Add bug detail page.
    And add bug search button.

--- static/js/site.js
+++ static/js/site.js
@@ -2,5 +2,25 @@
     "use strict";
     $(function(){
         $('#browserid').tooltip();
+        $('#shift-refresh').on('click', function(e){
+            e.preventDefault();
+            window.location.reload(true);
+        });
+        var $bug_id_search = $('#bug_id_search');
+        var $bug_id_search_btn = $('#bug_id_search_btn');
+        $bug_id_search_btn.on('click', function(e){
+            e.preventDefault();
+            var bugID = $bug_id_search.val();
+            window.location.href = '/b/' + bugID + '/';
+        });
+        $('#bug_id_search_modal').on('shown', function(){
+            $bug_id_search.focus();
+        });
+        $bug_id_search.on('keyup', function(e){
+            if (e.which === 13) {
+                e.preventDefault();
+                $bug_id_search_btn.trigger('click');
+            }
+        })
     });
 })(jQuery);
