87715489fb6ffe78cd24625ad4baabe23ac90fce
Paul McLanahan <pmac@mozilla.com>
2013-09-05 15:41:51 -0400

    Add clean_bugmail_logs as a cron task.

--- bugmail/cron.py
+++ bugmail/cron.py
@@ -1,9 +1,15 @@
 from cronjobs import register
 
-from bugmail.tasks import get_bugmail_messages
+from bugmail.tasks import clean_bugmail_log, get_bugmail_messages
 
 
 @register
 def get_bugmails():
     """Cron version of the periodic celery task."""
     get_bugmail_messages()
+
+
+@register
+def clean_bugmails():
+    """Cron version of the periodic celery task."""
+    clean_bugmail_log()
