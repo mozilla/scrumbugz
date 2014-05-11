645f839b7fec3ef05cea5fbff5caba104fc7b3df
Paul McLanahan <pmac@mozilla.com>
2014-05-10 22:57:03 -0400

    Add WhiteNoise for serving static media.

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-import sys, os
+import os
+import sys
+
 
 sys.path.append(os.path.dirname(__file__))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
